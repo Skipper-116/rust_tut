5c0b850f6f6a2c4f1672e583e22b42ea436313d2
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-11 18:53:51 +0200

    learnt variable, scopes, name shadowing and const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::string;
+
 fn main() {
-    println!("Hello, world!");
+    // negative numbers
+    const SOME_WEIRD_AMOUNT: u64 = 922337203608;
+    printer(SOME_WEIRD_AMOUNT);
+    let  x = 4; // x: i32  
+    printer(x);
+    {
+        let  x = x - 1; // x: i32
+        printer(x);
+    }
+    let x = 5;
+    printer(x);
+    printer(x + 1);
 }
+
+fn printer(x: u64) {
+    println!("x is: {}", x);
+}
+
